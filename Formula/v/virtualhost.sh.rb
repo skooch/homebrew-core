3133288c0928798b8b9439b008fb3a64c942644a
Michael Cho <michael@michaelcho.dev>
2024-08-11 22:10:30 -0400

    virtualhost.sh 2.0.0.beta2

--- Formula/v/virtualhost.sh.rb
+++ Formula/v/virtualhost.sh.rb
@@ -1,9 +1,11 @@
 class VirtualhostSh < Formula
   desc "Script for macOS to create Apache virtual hosts"
   homepage "https://github.com/virtualhost/virtualhost.sh"
-  url "https://github.com/virtualhost/virtualhost.sh/archive/refs/tags/1.35.tar.gz"
-  sha256 "75d34b807e71acd253876c6a99cdbc11ce31ffb159155373c83a99af862fffcc"
-  head "https://github.com/virtualhost/virtualhost.sh.git", branch: "master"
+  # We use beta release as stable/1.x releases have no license
+  url "https://github.com/virtualhost/virtualhost.sh/archive/refs/tags/2.0.0.beta2.tar.gz"
+  sha256 "7413109605b7f1f508102216134738cbdd4e3b23b2dec85d89f63afd1ab34bdb"
+  license "MIT"
+  head "https://github.com/virtualhost/virtualhost.sh.git", branch: "2.x"
 
   bottle do
     rebuild 1
